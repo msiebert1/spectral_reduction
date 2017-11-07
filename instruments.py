fe571b6087fa7c6a824b4d46015311c76c8b31d0
Matt Siebert <siebertmatt@gmail.com>
2017-11-06 16:50:00 -0800

    First commit

---
+++ instruments.py
@@ -0,0 +1,8 @@
+kast_blue = {'read_noise': 2.1,
+			 'gain': 5.4,
+			 'grism': 'temp',
+			 'filter': 'temp',
+			 'slit': 'temp',
+			 'dispaxis': 1,
+			 'biassec': '[328:443,322:344]',
+			 'trimsec': '[1:2048,1:350]'}
